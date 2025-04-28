35c940935c88cef500c1916feb92af6742b7f86c
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-04-28 14:13:51 +0700

    Menambahkan validasi di setiap Inputfield

--- lib/barang/pendataan_barang.dart
+++ lib/barang/pendataan_barang.dart
@@ -17,6 +17,8 @@ class PendataanBarangPage extends StatefulWidget {
 }
 
 class _PendataanBarangPageState extends State<PendataanBarangPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   final TextEditingController tanggalController = TextEditingController();
   final TextEditingController jumlahBarangController = TextEditingController();
   final TextEditingController hargaSatuanController = TextEditingController();
@@ -47,151 +49,155 @@ class _PendataanBarangPageState extends State<PendataanBarangPage> {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            buildLabel('Tanggal Transaksi'),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: tanggalController,
-              readOnly: true,
-              onTap: () async {
-                DateTime? pickedDate = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(2000),
-                  lastDate: DateTime(2101),
-                );
-                if (pickedDate != null) {
-                  tanggalController.text =
-                      "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
-                }
-              },
-              decoration: InputDecoration(
-                hintText: 'Tanggal Transaksi',
-                prefixIcon: const Icon(
-                  Icons.calendar_today,
-                  color: Colors.black54,
-                ),
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 20,
-                  horizontal: 16,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: const BorderSide(color: Colors.black26),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: const BorderSide(color: Colors.black26),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                  borderSide: const BorderSide(color: Colors.redAccent),
-                ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            buildDropdownField(
-              label: 'Jenis Transaksi',
-              value: selectedJenisTransaksi,
-              items: jenisTransaksiList,
-              onChanged: (value) {
-                setState(() {
-                  selectedJenisTransaksi = value;
-                });
-              },
-            ),
-            const SizedBox(height: 16),
-            buildDropdownField(
-              label: 'Jenis Barang',
-              value: selectedJenisBarang,
-              items: jenisBarangList,
-              onChanged: (value) {
-                setState(() {
-                  selectedJenisBarang = value;
-                });
-              },
-            ),
-            const SizedBox(height: 24),
-            Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      buildLabel('Jumlah Barang'),
-                      const SizedBox(height: 8),
-                      buildInputField(
-                        controller: jumlahBarangController,
-                        hintText: 'Jumlah Barang',
-                      ),
-                    ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              buildLabel('Tanggal Transaksi'),
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: tanggalController,
+                readOnly: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Tanggal transaksi tidak boleh kosong';
+                  }
+                  return null;
+                },
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2101),
+                  );
+                  if (pickedDate != null) {
+                    tanggalController.text =
+                        "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+                  }
+                },
+                decoration: buildInputDecoration(
+                  hintText: 'Tanggal Transaksi',
+                  prefixIcon: const Icon(
+                    Icons.calendar_today,
+                    color: Colors.black54,
                   ),
                 ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      buildLabel('Harga Satuan'),
-                      const SizedBox(height: 8),
-                      TextFormField(
-                        controller: hargaSatuanController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                          prefixText: 'Rp. ',
-                          hintText: 'Harga Satuan',
-                          filled: true,
-                          fillColor: Colors.white,
-                          contentPadding: const EdgeInsets.symmetric(
-                            vertical: 20,
-                            horizontal: 16,
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: const BorderSide(color: Colors.black26),
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: const BorderSide(color: Colors.black26),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            borderSide: const BorderSide(
-                              color: Colors.redAccent,
-                            ),
+              ),
+              const SizedBox(height: 16),
+              buildDropdownField(
+                label: 'Jenis Transaksi',
+                value: selectedJenisTransaksi,
+                items: jenisTransaksiList,
+                onChanged: (value) {
+                  setState(() {
+                    selectedJenisTransaksi = value;
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Pilih jenis transaksi';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16),
+              buildDropdownField(
+                label: 'Jenis Barang',
+                value: selectedJenisBarang,
+                items: jenisBarangList,
+                onChanged: (value) {
+                  setState(() {
+                    selectedJenisBarang = value;
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Pilih jenis barang';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 24),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        buildLabel('Jumlah Barang'),
+                        const SizedBox(height: 8),
+                        TextFormField(
+                          controller: jumlahBarangController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Jumlah barang tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                          decoration: buildInputDecoration(
+                            hintText: 'Jumlah Barang',
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 32),
-            SizedBox(
-              width: double.infinity,
-              height: 56,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.redAccent,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        buildLabel('Harga Satuan'),
+                        const SizedBox(height: 8),
+                        TextFormField(
+                          controller: hargaSatuanController,
+                          keyboardType: TextInputType.number,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Jumlah barang tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                          decoration: buildInputDecoration(
+                            hintText: 'Harga Satuan',
+                            prefixText: 'Rp. ',
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                onPressed: () {
-                  // Action Submit
-                },
-                child: const Text(
-                  'Submit',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ],
+              ),
+              const SizedBox(height: 32),
+              SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.redAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Semua validasi sukses
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Data berhasil disubmit!'),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text(
+                    'Submit',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -205,38 +211,12 @@ Widget buildLabel(String text) {
   );
 }
 
-Widget buildInputField({
-  required TextEditingController controller,
-  required String hintText,
-}) {
-  return TextFormField(
-    controller: controller,
-    decoration: InputDecoration(
-      hintText: hintText,
-      filled: true,
-      fillColor: Colors.white,
-      contentPadding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: const BorderSide(color: Colors.black26),
-      ),
-      enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: const BorderSide(color: Colors.black26),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: const BorderSide(color: Colors.redAccent),
-      ),
-    ),
-  );
-}
-
 Widget buildDropdownField({
   required String label,
   required String? value,
   required List<String> items,
   required Function(String?) onChanged,
+  String? Function(String?)? validator,
 }) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
@@ -245,33 +225,41 @@ Widget buildDropdownField({
       const SizedBox(height: 8),
       DropdownButtonFormField<String>(
         value: value,
+        validator: validator,
         items:
             items
                 .map((item) => DropdownMenuItem(value: item, child: Text(item)))
                 .toList(),
         onChanged: onChanged,
-        decoration: InputDecoration(
-          hintText: label,
-          filled: true,
-          fillColor: Colors.white,
-          contentPadding: const EdgeInsets.symmetric(
-            vertical: 20,
-            horizontal: 16,
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: const BorderSide(color: Colors.black26),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: const BorderSide(color: Colors.black26),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: const BorderSide(color: Colors.redAccent),
-          ),
-        ),
+        decoration: buildInputDecoration(hintText: label),
       ),
     ],
+  );
+}
+
+InputDecoration buildInputDecoration({
+  required String hintText,
+  Widget? prefixIcon,
+  String? prefixText,
+}) {
+  return InputDecoration(
+    hintText: hintText,
+    prefixIcon: prefixIcon,
+    prefixText: prefixText,
+    filled: true,
+    fillColor: Colors.white,
+    contentPadding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: const BorderSide(color: Colors.black26),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: const BorderSide(color: Colors.black26),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: const BorderSide(color: Colors.redAccent),
+    ),
   );
 }
