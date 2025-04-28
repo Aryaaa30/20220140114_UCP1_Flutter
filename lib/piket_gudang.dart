1f353c60c51f105935efc87c2b158a60fe60af75
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-04-28 12:15:43 +0700

    Menambahkan validasi di PiketGudangPage

--- lib/piket_gudang.dart
+++ lib/piket_gudang.dart
@@ -15,6 +15,8 @@ class _PiketGudangPageState extends State<PiketGudangPage> {
   final TextEditingController tanggalController = TextEditingController();
   final TextEditingController tugasPiketController = TextEditingController();
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   final List<Map<String, String>> daftarTugas = [];
 
   @override
@@ -25,9 +27,7 @@ class _PiketGudangPageState extends State<PiketGudangPage> {
         backgroundColor: Colors.redAccent,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+          onPressed: () => Navigator.pop(context),
         ),
         title: const Text(
           'Piket Gudang',
@@ -37,120 +37,138 @@ class _PiketGudangPageState extends State<PiketGudangPage> {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 16),
-            buildLabel('Nama Anggota'),
-            buildInputField(namaAnggotaController, readOnly: true),
-            const SizedBox(height: 16),
-            buildLabel('Pilih Tanggal'),
-            buildInputField(
-              tanggalController,
-              readOnly: true,
-              suffixIcon: const Icon(Icons.calendar_today),
-              onTap: () async {
-                DateTime? pickedDate = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(2000),
-                  lastDate: DateTime(2101),
-                );
-                if (pickedDate != null) {
-                  tanggalController.text =
-                      "${pickedDate.day}, ${pickedDate.month}, ${pickedDate.year}";
-                }
-              },
-            ),
-            const SizedBox(height: 16),
-            buildLabel('Tugas Piket'),
-            Row(
-              children: [
-                Expanded(child: buildInputField(tugasPiketController)),
-                const SizedBox(width: 8),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.redAccent,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 24,
-                      vertical: 20,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  onPressed: () {
-                    if (tugasPiketController.text.isNotEmpty) {
-                      setState(() {
-                        daftarTugas.add({
-                          'nama': namaAnggotaController.text,
-                          'tanggal': tanggalController.text,
-                          'tugas': tugasPiketController.text,
-                        });
-                        tugasPiketController.clear();
-                      });
-                    }
-                  },
-                  child: const Text(
-                    'Tambah',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 32),
-            const Center(
-              child: Text(
-                'Daftar Tugas Piket',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 16),
+              buildLabel('Nama Anggota'),
+              buildInputField(
+                namaAnggotaController,
+                readOnly: true,
+                isTanggal: false,
+                isTugas: false,
               ),
-            ),
-            const SizedBox(height: 16),
-            daftarTugas.isEmpty
-                ? const Center(child: Text('Belum ada Data'))
-                : ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: daftarTugas.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder:
-                                (context) => DetailMenyapuPage(
-                                  nama: daftarTugas[index]['nama'] ?? '',
-                                  tanggal: daftarTugas[index]['tanggal'] ?? '',
-                                  tugas: daftarTugas[index]['tugas'] ?? '',
-                                ),
+              const SizedBox(height: 16),
+              buildLabel('Pilih Tanggal'),
+              buildInputField(
+                tanggalController,
+                readOnly: true,
+                suffixIcon: const Icon(Icons.calendar_today),
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2101),
+                  );
+                  if (pickedDate != null) {
+                    tanggalController.text =
+                        "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+                  }
+                },
+                isTanggal: true,
+                isTugas: false,
+              ),
+              const SizedBox(height: 16),
+              buildLabel('Tugas Piket'),
+              Row(
+                children: [
+                  Expanded(
+                    child: buildInputField(
+                      tugasPiketController,
+                      isTanggal: false,
+                      isTugas: true,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.redAccent,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 20,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        setState(() {
+                          daftarTugas.add({
+                            'nama': namaAnggotaController.text,
+                            'tanggal': tanggalController.text,
+                            'tugas': tugasPiketController.text,
+                          });
+                          tugasPiketController.clear();
+                          tanggalController.clear();
+                        });
+                      }
+                    },
+                    child: const Text(
+                      'Tambah',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 32),
+              const Center(
+                child: Text(
+                  'Daftar Tugas Piket',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 16),
+              daftarTugas.isEmpty
+                  ? const Center(child: Text('Belum ada Data'))
+                  : ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: daftarTugas.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder:
+                                  (context) => DetailMenyapuPage(
+                                    nama: daftarTugas[index]['nama'] ?? '',
+                                    tanggal:
+                                        daftarTugas[index]['tanggal'] ?? '',
+                                    tugas: daftarTugas[index]['tugas'] ?? '',
+                                  ),
+                            ),
+                          );
+                        },
+                        child: Card(
+                          color: Colors.redAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
                           ),
-                        );
-                      },
-                      child: Card(
-                        color: Colors.redAccent,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        margin: const EdgeInsets.symmetric(vertical: 8),
-                        child: ListTile(
-                          title: Text(
-                            daftarTugas[index]['tugas'] ?? '',
-                            style: const TextStyle(
+                          margin: const EdgeInsets.symmetric(vertical: 8),
+                          child: ListTile(
+                            title: Text(
+                              daftarTugas[index]['tugas'] ?? '',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            trailing: const Icon(
+                              Icons.arrow_forward_ios,
                               color: Colors.white,
-                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                          trailing: const Icon(
-                            Icons.arrow_forward_ios,
-                            color: Colors.white,
-                          ),
                         ),
-                      ),
-                    );
-                  },
-                ),
-          ],
+                      );
+                    },
+                  ),
+            ],
+          ),
         ),
       ),
     );
@@ -169,11 +187,23 @@ Widget buildInputField(
   bool readOnly = false,
   Widget? suffixIcon,
   VoidCallback? onTap,
+  bool isTanggal = false,
+  bool isTugas = false,
 }) {
   return TextFormField(
     controller: controller,
     readOnly: readOnly,
     onTap: onTap,
+    validator: (value) {
+      if (value == null || value.isEmpty) {
+        if (isTanggal) {
+          return 'Tanggal tidak boleh kosong!';
+        } else if (isTugas) {
+          return 'Tugas tidak boleh kosong!';
+        }
+      }
+      return null;
+    },
     decoration: InputDecoration(
       filled: true,
       fillColor: Colors.white,
