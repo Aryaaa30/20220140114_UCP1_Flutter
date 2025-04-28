9407bddb07c2602b57c7837aac03afd0528ff867
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-04-28 14:31:52 +0700

    Menambahkan ikon sukses pada DetailBarangPage

--- lib/barang/detail_barang.dart
+++ lib/barang/detail_barang.dart
@@ -18,6 +18,60 @@ class DetailDataPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final int totalHarga = jumlahBarang * hargaSatuan;
+    final currencyFormat = NumberFormat.currency(
+      locale: 'id_ID',
+      symbol: 'Rp. ',
+      decimalDigits: 0,
+    );
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFFCF6F3),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 32),
+              Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green.withOpacity(0.2),
+                ),
+                child: const CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Colors.green,
+                  child: Icon(Icons.check, color: Colors.white, size: 48),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+}
+
+Widget buildDetailRow(String title, String value) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+        ),
+        Flexible(
+          child: Text(
+            value,
+            textAlign: TextAlign.end,
+            style: const TextStyle(fontSize: 14),
+          ),
+        ),
+      ],
+    ),
+  );
 }
