095b35faa8f989bc40e849536ac4989b1fa4768c
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-04-28 20:52:26 +0700

    Menambahkan Design pada DataPelangganPage

--- lib/detail_menyapu_page.dart
+++ lib/detail_menyapu_page.dart
@@ -17,80 +17,116 @@ class DetailMenyapuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFDF6F3),
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+      // Ganti background color menjadi gradient seperti LoginPage
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 174, 141, 80), // krem kayu
+              Color(0xFFDEB887), // coklat muda
+              Color(0xFF8B4513), // coklat tua
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
         ),
-        title: const Text(
-          'Detail Menyapu',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16),
+            AppBar(
+              backgroundColor: const Color.fromARGB(
+                255,
+                139,
+                69,
+                19,
+              ), // coklat tua
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.white),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      tanggal,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        color: Colors.redAccent,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.redAccent,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Text(
-                      nama,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
+              title: const Text(
+                'Detail Menyapu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
+              centerTitle: true,
+              elevation: 0,
+              automaticallyImplyLeading: false,
             ),
-            const SizedBox(height: 24),
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.redAccent,
-                borderRadius: BorderRadius.circular(24),
-              ),
-              child: Center(
-                child: Text(
-                  tugas,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              tanggal,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.brown,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 8,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(
+                                255,
+                                139,
+                                69,
+                                19,
+                              ), // coklat tua
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Text(
+                              nama,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 139, 69, 19), // coklat tua
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      child: Center(
+                        child: Text(
+                          tugas,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
--- lib/pelanggan/data_pelanggan_page.dart
+++ lib/pelanggan/data_pelanggan_page.dart
@@ -30,9 +30,8 @@ class _DataPelangganPageState extends State<DataPelangganPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFCF6F3),
       appBar: AppBar(
-        backgroundColor: Colors.redAccent,
+        backgroundColor: const Color(0xFF8B4513),
         title: const Text(
           'Data Pelanggan',
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
@@ -45,185 +44,204 @@ class _DataPelangganPageState extends State<DataPelangganPage> {
           },
         ),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              buildLabel('Nama Cust'),
-              buildInputField(
-                controller: namaController,
-                hintText: 'Nama Cust',
-              ),
-              const SizedBox(height: 16),
-              Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        buildLabel('Email'),
-                        buildInputField(
-                          controller: emailController,
-                          hintText: 'Email',
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        buildLabel('No Hp'),
-                        buildInputField(
-                          controller: noHpController,
-                          hintText: 'No Hp',
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              buildLabel('Alamat'),
-              buildInputField(controller: alamatController, hintText: 'Alamat'),
-              const SizedBox(height: 16),
-              Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        buildLabel('Provinsi'),
-                        buildInputField(
-                          controller: provinsiController,
-                          hintText: 'Provinsi',
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        buildLabel('Kode Pos'),
-                        buildInputField(
-                          controller: kodePosController,
-                          hintText: 'Kode Pos',
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 32),
-              SizedBox(
-                width: double.infinity,
-                height: 56,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.redAccent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                  ),
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder:
-                              (context) => DetailPelangganPage(
-                                nama: namaController.text,
-                                email: emailController.text,
-                                noHp: noHpController.text,
-                                alamat: alamatController.text,
-                                provinsi: provinsiController.text,
-                                kodePos: kodePosController.text,
-                              ),
-                        ),
-                      );
-                    }
-                  },
-                  child: const Text('Simpan', style: TextStyle(fontSize: 16)),
-                ),
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                height: 56,
-                child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    side: const BorderSide(color: Colors.redAccent),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                  ),
-                  onPressed: () {
-                    namaController.clear();
-                    emailController.clear();
-                    noHpController.clear();
-                    alamatController.clear();
-                    provinsiController.clear();
-                    kodePosController.clear();
-                  },
-                  child: const Text(
-                    'Reset',
-                    style: TextStyle(color: Colors.redAccent, fontSize: 16),
-                  ),
-                ),
-              ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 174, 141, 80),
+              Color(0xFFDEB887),
+              Color(0xFF8B4513),
             ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                buildLabel('Nama Cust'),
+                buildInputField(
+                  controller: namaController,
+                  hintText: 'Nama Cust',
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          buildLabel('Email'),
+                          buildInputField(
+                            controller: emailController,
+                            hintText: 'Email',
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          buildLabel('No Hp'),
+                          buildInputField(
+                            controller: noHpController,
+                            hintText: 'No Hp',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                buildLabel('Alamat'),
+                buildInputField(
+                  controller: alamatController,
+                  hintText: 'Alamat',
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          buildLabel('Provinsi'),
+                          buildInputField(
+                            controller: provinsiController,
+                            hintText: 'Provinsi',
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          buildLabel('Kode Pos'),
+                          buildInputField(
+                            controller: kodePosController,
+                            hintText: 'Kode Pos',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 32),
+                SizedBox(
+                  width: double.infinity,
+                  height: 56,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF8B4513),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder:
+                                (context) => DetailPelangganPage(
+                                  nama: namaController.text,
+                                  email: emailController.text,
+                                  noHp: noHpController.text,
+                                  alamat: alamatController.text,
+                                  provinsi: provinsiController.text,
+                                  kodePos: kodePosController.text,
+                                ),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text(
+                      'Simpan',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  height: 56,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(color: Colors.white),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: () {
+                      namaController.clear();
+                      emailController.clear();
+                      noHpController.clear();
+                      alamatController.clear();
+                      provinsiController.clear();
+                      kodePosController.clear();
+                    },
+                    child: const Text(
+                      'Reset',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
-}
 
-Widget buildLabel(String text) {
-  return Padding(
-    padding: const EdgeInsets.only(bottom: 8),
-    child: Text(
-      text,
-      style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-    ),
-  );
-}
+  Widget buildLabel(String text) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+          color: Colors.brown,
+        ),
+      ),
+    );
+  }
 
-Widget buildInputField({
-  required TextEditingController controller,
-  required String hintText,
-}) {
-  return TextFormField(
-    controller: controller,
-    decoration: InputDecoration(
-      hintText: hintText,
-      filled: true,
-      fillColor: Colors.white,
-      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: const BorderSide(color: Colors.black26),
+  Widget buildInputField({
+    required TextEditingController controller,
+    required String hintText,
+  }) {
+    return TextFormField(
+      controller: controller,
+      style: const TextStyle(color: Colors.black),
+      decoration: InputDecoration(
+        hintText: hintText,
+        labelStyle: const TextStyle(color: Colors.brown),
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 18,
+        ),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
       ),
-      enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: const BorderSide(color: Colors.black26),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: const BorderSide(color: Colors.redAccent),
-      ),
-    ),
-    validator: (value) {
-      if (value == null || value.isEmpty) {
-        return '$hintText tidak boleh kosong';
-      }
-      return null;
-    },
-  );
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return '$hintText tidak boleh kosong';
+        }
+        return null;
+      },
+    );
+  }
 }
