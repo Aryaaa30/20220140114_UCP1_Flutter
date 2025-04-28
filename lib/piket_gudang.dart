41fdd3b540c5041caf74eecc8c0a06e2487dc4cd
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-04-28 11:35:28 +0700

    Menambahkan inputfield Nama Anggota dan Tanggal

--- lib/piket_gudang.dart
+++ lib/piket_gudang.dart
@@ -16,4 +16,81 @@ class _PiketGudangPageState extends State<PiketGudangPage> {
   final TextEditingController tugasPiketController = TextEditingController();
 
   final List<Map<String, String>> daftarTugas = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFDF6F3),
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          'Piket Gudang',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 16),
+            buildLabel('Nama Anggota'),
+            buildInputField(namaAnggotaController, readOnly: true),
+            const SizedBox(height: 16),
+            buildLabel('Pilih Tanggal'),
+            buildInputField(
+              tanggalController,
+              readOnly: true,
+              suffixIcon: const Icon(Icons.calendar_today),
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2101),
+                );
+                if (pickedDate != null) {
+                  tanggalController.text =
+                      "${pickedDate.toLocal()}".split(' ')[0];
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildLabel(String text) {
+  return Text(
+    text,
+    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+  );
+}
+
+Widget buildInputField(
+  TextEditingController controller, {
+  bool readOnly = false,
+  Widget? suffixIcon,
+  VoidCallback? onTap,
+}) {
+  return TextFormField(
+    controller: controller,
+    readOnly: readOnly,
+    onTap: onTap,
+    decoration: InputDecoration(
+      filled: true,
+      fillColor: Colors.white,
+      suffixIcon: suffixIcon,
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  );
 }
