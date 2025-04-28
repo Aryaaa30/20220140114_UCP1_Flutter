75d1214ddbc875926eb119a6915f8b3991e3a9a5
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-04-28 14:27:04 +0700

    Membuat DetailBarangPage

---
+++ lib/barang/detail_barang.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class DetailDataPage extends StatelessWidget {
+  final String tanggal;
+  final String jenisTransaksi;
+  final String jenisBarang;
+  final int jumlahBarang;
+  final int hargaSatuan;
+
+  const DetailDataPage({
+    super.key,
+    required this.tanggal,
+    required this.jenisTransaksi,
+    required this.jenisBarang,
+    required this.jumlahBarang,
+    required this.hargaSatuan,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
